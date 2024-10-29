322d826dae8f20eb9513aa5e2a1f656fe8e7c539
NIU1633814 <1633814@uab.cat>
2024-10-29 17:34:15 +0100

    funcionalidad basica dado hecha

--- src/test/java/uab/tqs/parchis/model/DadoTest.java
+++ src/test/java/uab/tqs/parchis/model/DadoTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TestDado {
+class DadoTest {
 
     private Dado dado;
 
@@ -14,7 +14,7 @@ public class TestDado {
     }
 
     @Test
-    public void testTirar() {
+    void testTirar() {
         int resultado = dado.tirar();
 
         assertTrue(resultado >= 1 && resultado <= 6, "El número debería estar entre 1 y 6, pero fue: " + resultado);
