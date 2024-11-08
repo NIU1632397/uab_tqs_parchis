9b2e32a4cd469e8ee57a82f249b08063a3b03f7a
NIU1633814 <1633814@uab.cat>
2024-11-08 15:25:51 +0100

    Test iniciales casilla

---
+++ src/test/java/uab/tqs/parchis/model/TestCasilla.java
@@ -0,0 +1,38 @@
+package uab.tqs.parchis.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestCasilla {
+
+    private CasillaCasa casilla_casa;
+
+    @BeforeEach
+    void setUp() {
+        casilla_casa = new CasillaCasa(0);
+    }
+
+    @Test
+    void testNumero() {
+        assertEquals(0, casilla_casa.getNumero(), "El número de casilla deberia ser 0");
+    }
+
+    @Test
+    void testSeguro() {
+        assertTrue(casilla_casa.getSeguro(), "La casilla deberia ser segura");
+    }
+
+    @Test
+    void testAgregarQuitar() {
+        Ficha ficha = new Ficha();
+
+        casilla_casa.agregarFicha(ficha);
+
+        assertFalse(casilla_casa.getFichas().isEmpty(), "Deberia haber alguna ficha en la casilla");
+
+        casilla_casa.quitarFicha(ficha);
+
+        assertTrue(casilla_casa.getFichas().isEmpty(), "no deberia haber ninguna ficha en la casilla");
+    }
+}
